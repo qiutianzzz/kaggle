52a7adb3a7ebe0146ace31e11135bcc7cee5a8d3
qiutianzzz <lingquan_zhang@163.com>
2020-04-02 23:21:07 +0800

    update the file to set missing values

--- house_p/data_pro.py
+++ house_p/data_pro.py
@@ -13,38 +13,38 @@ data_train.info()
 # BldgType, HouseStyle, YearBuilt, YearRemodAdd, RoofStyle, RoofMatl, MasVnrArea, Foundation
 # Heating, GarageQual, PoolQC, SaleType, SaleCondition, YrSold
 
-# def set_missing_ages(df):
+def set_missing_ages(df):
     
-#     # 把已有的数值型特征取出来丢进Random Forest Regressor中
-#     age_df = df[['Age','Fare', 'Parch', 'SibSp', 'Pclass']]
+    # 把已有的数值型特征取出来丢进Random Forest Regressor中
+    age_df = df[['Age','Fare', 'Parch', 'SibSp', 'Pclass']]
 
 
-#     # 乘客分成已知年龄和未知年龄两部分
-#     known_age = age_df[age_df.Age.notnull()].values
-#     unknown_age = age_df[age_df.Age.isnull()].values
+    # 乘客分成已知年龄和未知年龄两部分
+    known_age = age_df[age_df.Age.notnull()].values
+    unknown_age = age_df[age_df.Age.isnull()].values
     
-#     # y即目标年龄
-#     y = known_age[:, 0]
-#     print('the data in y:', y)
-#     # X即特征属性值
-#     X = known_age[:, 1:]
-#     print('the data in X', X)
+    # y即目标年龄
+    y = known_age[:, 0]
+    print('the data in y:', y)
+    # X即特征属性值
+    X = known_age[:, 1:]
+    print('the data in X', X)
 
-#     # fit到RandomForestRegressor之中
-#     rfr = RandomForestRegressor(random_state=0, n_estimators=2000, n_jobs=-1)
-#     rfr.fit(X, y)
+    # fit到RandomForestRegressor之中
+    rfr = RandomForestRegressor(random_state=0, n_estimators=2000, n_jobs=-1)
+    rfr.fit(X, y)
     
-#     # 用得到的模型进行未知年龄结果预测
-#     predictedAges = rfr.predict(unknown_age[:, 1:])
+    # 用得到的模型进行未知年龄结果预测
+    predictedAges = rfr.predict(unknown_age[:, 1:])
 
-#     print('-------------------TEST------------------------')
+    print('-------------------TEST------------------------')
 
-#     print('-------------------TEST------------------------')
+    print('-------------------TEST------------------------')
     
-#     # 用得到的预测结果填补原缺失数据
-#     df.loc[ (df.Age.isnull()), 'Age' ] = predictedAges 
+    # 用得到的预测结果填补原缺失数据
+    df.loc[ (df.Age.isnull()), 'Age' ] = predictedAges 
     
-#     return df, rfr
+    return df, rfr
 
 
 # scaler = preprocessing.StandardScaler()
